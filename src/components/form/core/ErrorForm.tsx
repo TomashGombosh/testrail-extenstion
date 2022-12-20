1c87cdb6badb25f5e6ece8d4d85f96dae6d190ec
Tomash Gombosh <tomash.gombosh@idealscorp.com>
2022-12-20 19:07:26 +0200

    [BUG] Fix teams bug

--- src/components/form/core/ErrorForm.tsx
+++ src/components/form/core/ErrorForm.tsx
@@ -9,16 +9,15 @@ export type ErrorProps = {
 }
 
 const ErrorForm = ({errorMessage, retryFunction}: ErrorProps) => {
-  const errorContent =
-    <Grid item>
+  const errorContent = (
+    <><Grid item>
       <span id="error">{errorMessage}</span>
-    </Grid>;
-  <Grid item>
-    <CustomButton disabled={true} handleClick={retryFunction || (() => console.log("Click"))} text="Retry"/>
-  </Grid>;
-  <Grid item>
-    <LinkButton link="/" text="Back"/>
-  </Grid>;
+    </Grid><Grid item>
+      <CustomButton disabled={true} handleClick={retryFunction || (() => console.log("Click"))} text="Retry" />
+    </Grid>
+    <Grid item>
+      <LinkButton link="/" text="Back"/>
+    </Grid></>);
 
   return <Form header="Something went wrong" content={errorContent}/>;
 };
