25f19f8137809005342eb4b94ee62bce0b40ded9
TomashGombosh <blackjneco@gmail.com>
2022-12-20 11:38:12 +0200

    [UI] Update error form

--- src/components/form/core/ErrorForm.tsx
+++ src/components/form/core/ErrorForm.tsx
@@ -1,17 +1,22 @@
 import React from "react";
 import Grid from "@mui/material/Grid";
 import Form from "./Form";
+import CustomButton from "../../button/Button";
 export type ErrorProps = {
-    errorMessage: string
+    errorMessage: string,
+    retryFunction(): void
 }
 
-const ErrorForm = (props: ErrorProps) => {
+const ErrorForm = ({errorMessage, retryFunction}: ErrorProps) => {
   const errorContent =
     <Grid item>
-      <span id="error">{props.errorMessage}</span>
+      <span id="error">{errorMessage}</span>
     </Grid>;
+  <Grid item>
+    <CustomButton disabled={true} handleClick={retryFunction} text="Retry"/>
+  </Grid>;
 
-  return <Form header="Something went woring" content={errorContent}/>;
+  return <Form header="Something went wrong" content={errorContent}/>;
 };
 
 export default ErrorForm;
